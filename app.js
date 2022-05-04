1254800fdd4bb8dcab1002e457e5108852f63f31
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-04 12:48:37 -0700

    display mushroom function

--- app.js
+++ app.js
@@ -80,9 +80,11 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl.textContent = '';
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroomEl = renderMushroom(1);
+        mushroomEl.append(mushroomEl);
     }
 }
 
